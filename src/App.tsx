fa6ce35911ab8d0a7949f98bf56ed4897a05bdcd
Boris Traljić <btraljic@gmail.com>
2021-03-12 18:27:49 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-sm'>One of three columns</div>
+        <div className='col-sm'>One of three columns</div>
+        <div className='col-sm'>One of three columns</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
